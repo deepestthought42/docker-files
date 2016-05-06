5ec6b287e61cf597005626b094ac484bb87ac4c1
Renee Klawitter <deepestthought42@gmail.com>
2016-05-06 16:47:10 -0700

    initial commit

---
+++ sbcl/install.lisp
@@ -0,0 +1,7 @@
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2016-04-21/distinfo.txt")
+
+(load "quicklisp.lisp")
+
+(quicklisp-quickstart:install :path "/lisp/quicklisp/" :dist-url *dist-url*)
+
+
