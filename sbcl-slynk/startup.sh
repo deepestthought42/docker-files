9e68d0d06f14f79d7f0e3def7ce44c69c276e5c2
Renee Klawitter <deepestthought42@gmail.com>
2016-05-13 23:45:25 -0700

    sbcl-slynk: made port and src-dir dependent on env. variables
    change: does not start sbcl anymore

--- sbcl-slynk/startup.sh
+++ sbcl-slynk/startup.sh
@@ -3,6 +3,6 @@
 echo "Starting SSH daemon"
 service ssh start
 
-echo "Starting SBCL w/ slynk server"
-/usr/local/bin/sbcl --eval "(format t \"Started\")"
+# echo "Starting SBCL w/ slynk server"
+# /usr/local/bin/sbcl --eval "(format t \"Started\")"
 
