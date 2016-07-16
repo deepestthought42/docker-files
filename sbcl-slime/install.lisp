9990eabfb7d4954a4667d4624e453bd7898be4d1
Renee Klawitter <deepestthought42@gmail.com>
2016-07-16 13:50:06 -0700

    sbcl-slime Dockerfile is now based on c2ffi and includes everything else

--- sbcl-slime/install.lisp
+++ sbcl-slime/install.lisp
@@ -1,3 +1,11 @@
 ;;; install slynk packages needed
 
+(defvar *dist-url* "http://beta.quicklisp.org/dist/quicklisp/2016-04-21/distinfo.txt")
+
+(load "quicklisp.lisp")
+
+(quicklisp-quickstart:install :path "~/lisp/quicklisp/" :dist-url *dist-url*)
+
+
+
 (ql:quickload :swank)
